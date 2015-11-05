063c570ee27c5f60945dd991620b8742131c99db
Mike Weilgart <mike.weilgart@verticalsysadmin.com>
2015-11-05 10:59:02 -0800

    Fix runslides.sh installation logic and remove inaccurate comment

--- source/run_slides.sh
+++ source/run_slides.sh
@@ -2,9 +2,6 @@
 
 # Purpose: show cfengine examples using vim.  This allows us
 # to edit the examples during the presentation.
-#
-# Note: there is a .vimrc in this directory. You have to
-# have "set exrc" enabled for vim to load it automatically.
 
 
 # working with the training collection requires the following packages:
@@ -12,7 +9,7 @@
 for REQUIRED in asciidoc elinks vim elinks regex-markup
 do
   # check if it is installed, and if not, install it
-  [ -x `which ${REQUIRED}` ] || \
+  command -v ${REQUIRED} >/dev/null 2>&1 || \
   (
      # try to guess the package mgr
      sudo yum install ${REQUIRED} 2>/dev/null  || sudo apt-get install ${REQUIRED} 2>/dev/null
@@ -20,10 +17,10 @@ do
 done
 
 # pygments is used to colorize/syntax-highlight code
-[ -x `which pygmentize` ] || sudo yum install python-pygments || sudo apt-get install python-pygments
+command -v pygmentize >/dev/null 2>&1 || sudo yum install python-pygments || sudo apt-get install python-pygments
 
 # libreoffice-impress is used to display a presentation
-[ -x `which libreoffice` ] || sudo yum install libreoffice-impress || sudo apt-get install libreoffice-impress
+command -v libreoffice >/dev/null 2>&1 || sudo yum install libreoffice-impress || sudo apt-get install libreoffice-impress
 
 # start slideshow
 vim `ls -1 | egrep -v '.png$|.pdf$|.skip$|BOOKONLY' `
