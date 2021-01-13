30a7c377cef45c31b0c4fa80b32d194ea3206060
Aleksey Tsalolikhin <atsalolikhin@spokeo.com>
2021-01-12 22:18:56 -0800

    Make some updates

--- source/200-010-Introductory_overview-0030-Lifecycle_of_a_machine.CLASSONLY.sh
+++ source/200-010-Introductory_overview-0030-Lifecycle_of_a_machine.CLASSONLY.sh
@@ -4,4 +4,6 @@ echo 'Remy Evard, An Analysis of UNIX System Configuration'
 echo 'USENIX Proceedings: Eleventh Systems Administration Conference (LISA 1997), October 26-31, 1997'
 echo
 
-display 02-010-Introductory_overview-0035-lifecycle.png
+# Display diagram
+/mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe\
+  file://wsl%24/Ubuntu/home/atsaloli/git/cf3-tutorial/images/figures/lifecycle.png
