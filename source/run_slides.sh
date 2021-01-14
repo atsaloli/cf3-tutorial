c5a0a5f5101273b60cd4ed3db540d7e34570c5cd
Aleksey Tsalolikhin <atsalolikhin@spokeo.com>
2021-01-13 20:22:54 -0800

    Update pkg list -- remove obsolete pkgs

--- source/run_slides.sh
+++ source/run_slides.sh
@@ -6,7 +6,7 @@
 
 # working with the training collection requires the following packages:
 
-for REQUIRED in asciidoc elinks vim regex-markup
+for REQUIRED in vim regex-markup
 do
   # check if it is installed, and if not, install it
   command -v ${REQUIRED} >/dev/null 2>&1 || \
@@ -17,11 +17,10 @@ do
 done
 
 # pygments is used to colorize/syntax-highlight code
-command -v pygmentize >/dev/null 2>&1 || sudo yum install python-pygments || sudo apt-get install python-pygments
+command -v pygmentize >/dev/null 2>&1 || sudo yum install python3-pygments || sudo apt-get install python-pygments
 
 # libreoffice-impress is used to display a presentation
 command -v libreoffice >/dev/null 2>&1 || sudo yum install libreoffice-impress || sudo apt-get install libreoffice-impress
 
 # start slideshow
 vim `ls -1 | egrep -v '.png$|.pdf$|.skip$|BOOKONLY' `
-
