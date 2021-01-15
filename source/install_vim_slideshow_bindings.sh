57acfba9640450c61c928055099a48be29a8c986
Aleksey Tsalolikhin <aleksey@verticalsysadmin.com>
2021-01-15 13:48:00 +0000

    Update vim bindings installer
    - add installation of cf3 syntax highlighter
    - remove asciidoc

--- source/install_vim_slideshow_bindings.sh
+++ source/install_vim_slideshow_bindings.sh
@@ -5,16 +5,25 @@
 # - AsciiDoc syntax highlighter (the text portion of the "vim slideshow" is written in AsciiDoc) 
 
 
-cat <<EOF >> $HOME/.vimrc
+# First, install Neil Watson's cf3 syntax highlighter
+ls ./100-180-Installing_Syntax_Highlighter-0265-Install_Vim_Plugin.sh
+. ./100-180-Installing_Syntax_Highlighter-0265-Install_Vim_Plugin.sh
 
+# Now, add the slideshow bindings
+#
+# Note: it has to be in that order (cf3 syntax highligter first).
+# if you put the cf3 syntax highlighter below this section, it mucks up
+# the mdless colorization of headings for some reason
+cat <<EOF >> $HOME/.vimrc
 
 " The following are mappings to support the Training Examples "vim slideshow"
 
-" use F7 and BACKSPACE to control the run_slides.sh slideshow (move
+" use SPACE/F7 and BACKSPACE to control the run_slides.sh slideshow (move
 " forwards and backwards)
-" Note: it used to be <SPACE> and <BACKSPACE> but vim's pagination code
+" Note: at first, I used  <SPACE> and <BACKSPACE> but vim's pagination code
 " that gets invoked after a command is run eats spaces (that's the 'press
 " Enter to continue' prompt that vim gives you after running a command)
+" so I added F7 as a workaround for that
 map <F7> :next +1<CR>
 map <SPACE> :next +1<CR>
 map <BACKSPACE> :prev +1<CR>
@@ -34,9 +43,6 @@ set laststatus=2
 " Add file name to statusline so we know where we are in the slideshow
 set statusline+=%f
 
-" Syntax highlighting for AsciiDoc
-autocmd BufRead,BufNewFile *.txt set ft=asciidoc
-
 " render markdown to "t"ext with mdless (see below)
 map tt :term++curwin mdless --no-pager %:p
 
@@ -54,7 +60,8 @@ map tt :term++curwin mdless --no-pager %:p
 " We have to add "noautocmd" to prevent Vim from rendering it with mdless
 " as per the autocmd above.
 map ee :noautocmd e #
-" Colorize status bar (for working on cf3-tutorial)
+
+" Colorize status bar (to distinguish cf3, exercises and regular markdown files)
 " Markdown: black on green
 au BufRead *.md hi StatusLine ctermbg=black ctermfg=green
 " CF3: black on yellow
