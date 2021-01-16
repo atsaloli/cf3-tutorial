077b2b0aea6fbc621110cb82e89e6ea677c146e6
Aleksey Tsalolikhin <aleksey@verticalsysadmin.com>
2021-01-16 04:03:37 +0000

    Add die() function and pygmentize -L check for cf3 support

--- source/configure_vm.sh
+++ source/configure_vm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+die(){
+  echo "$*"
+  exit 1
+}
+
 # working with the training collection requires the following packages:
 
 for required_package in git wget
@@ -16,3 +21,5 @@ done
 command -v pygmentize >/dev/null 2>&1 || sudo yum install -y python3-pygments || sudo apt-get install -y python-pygments
 
 # also install ruby 2.6 (or newer) and then "gem install mdless" to install markdown->text converter
+# make sure "pygmentize -L | grep cf3" shows CFEngine is installed
+pygmentize -L | grep -q cf3 || die "'pygmentize -L' does not show cf3 (cfengine language support)"
