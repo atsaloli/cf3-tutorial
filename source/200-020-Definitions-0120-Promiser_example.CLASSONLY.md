c619223066f8164d72cbd3c6e326f6670ae06fae
Aleksey Tsalolikhin <aleksey@verticalsysadmin.com>
2021-01-16 06:43:44 +0000

    fix some stuff (including slideshow bindings or vimrc)

---
+++ source/200-020-Definitions-0120-Promiser_example.CLASSONLY.md
@@ -0,0 +1,11 @@
+
+The promiser follows the promise type, and is in double quotes.
+
+```cfengine3
+files:
+
+    "/etc/nologin"  
+
+        create  => "true",
+        comment => "Prevent non-root users from logging in";
+```
--- source/install_vim_slideshow_bindings.sh
+++ source/install_vim_slideshow_bindings.sh
@@ -16,7 +16,7 @@ ls ./100-180-Installing_Syntax_Highlighter-0265-Install_Vim_Plugin.sh
 # the mdless colorization of headings for some reason
 cat <<EOF >> $HOME/.vimrc
 
-" The following are mappings to support the Training Examples "vim slideshow"
+" ---- Start of mappings to support the Training Examples aka "vim slideshow"
 
 " use SPACE/F7 and BACKSPACE to control the run_slides.sh slideshow (move
 " forwards and backwards)
@@ -28,48 +28,83 @@ map <F7> :next +1<CR>
 map <SPACE> :next +1<CR>
 map <BACKSPACE> :prev +1<CR>
 
-" feed current "f"ile to cf-agent in Inform mode
-map ff :!clear;/var/cfengine/bin/cf-agent --color=always -KI -f '%:p'
+" feed "f"ile to cf-agent in Inform mode -- used when we are looking
+" at pygmentize output of cfengine policy source file, this allows us to run
+" the policy source file in cf-agent
+map ff :!clear;/var/cfengine/bin/cf-agent --color=always -KI -f '#:p'
 
-" feed current file to cf-agent in Verbose mode
-map vv :!clear;/var/cfengine/bin/cf-agent --color=always -KIv -f '%:p' \| less -R
+" feed file to cf-agent in Verbose mode
+map vv :!clear;/var/cfengine/bin/cf-agent --color=always -KIv -f '#:p' \| less -R
 
-" "r"un current file using /bin/sh
-map rr :!clear;/bin/sh '%:p'
+" "r"un file using /bin/sh
+map rr :!clear;/bin/sh '#:p'
 
+" Commenting out until
+" https://vi.stackexchange.com/questions/28752/vim-terminal-interferes-with-statusline?noredirect=1#comment52552_28752
+" is resolved
+"
 " Make status line visible
-set laststatus=2
-
-" Add file name to statusline so we know where we are in the slideshow
-set statusline+=%f
+" set laststatus=2
 
 " render markdown to "t"ext with mdless (see below)
-map tt :term++curwin mdless --no-pager %:p
-
 " mdless is a markdown to text converter: https://brettterpstra.com/projects/mdless/
 " Let's use it to render markdown formatting
 " We use Vim "terminal" because it can handle the escape codes
 " that mdless generates (it outputs color by default -- we could turn that off but
 " it looks better with color).
 " Install mdless with "gem install mdless" 
-:autocmd BufRead *.md :term++curwin mdless --no-pager --width=80 %:p
+map tt :term++curwin mdless --no-pager --width=100 %:p
+
+:autocmd BufRead *.md :term++curwin mdless --no-pager --width=100 %:p
 :autocmd BufRead *.cf :term++curwin pygmentize -l cfengine3 %:p
 
 " add a shortcut for "e"diting the previous file (alternate-file in Vim terminology).
-" This lets us edit the source markdown file.
-" We have to add "noautocmd" to prevent Vim from rendering it with mdless
-" as per the autocmd above.
+" This lets us edit the source markdown/cfengine file.
+" We have to add "noautocmd" to prevent Vim from rendering it with mdless/pygmentize
+" as per the autocmds above.
 map ee :noautocmd e #
+
+function! CustomStatusline()
+  " construct a custom statusline to show:
+  " - the buffer number of the alternate file (we will have just ran mdless or
+  "   pygmentize, so our buffer number will be different (higher)) -- i.e.,
+  "   this will tell us the buffer number of the original
+  "   file -- long story short, it gives us the file number, so we can tell
+  "   how far into the collection we are.
+  " - the total number of buffers (again, so we can tell where we are at -- e.g. 20 / 100)
+  " - this Function will also label Exercises as such so I don't blow past them in class
+
+  let current_buffer = bufnr('#')
+  let total_buffers = len(getbufinfo({'buflisted':1}))
+  let file_name = expand('%:t:r') " we'll use this to detect exercise files which are named *.exr.md (my own invention)
+  let extension = expand('%:e') " this can detect *.cf files if we should want to do something with it
+  if file_name =~ '^.*\.exr$'
+    let label = '     ------------------ Exercise ----------------------'
+    hi StatusLineTerm ctermbg=red ctermfg=black " change status bar color to something noticeable so exercises stand out
+  else
+    let label = ''
+    hi StatusLineTerm ctermbg=black ctermfg=white " white on black background makes status bar less intrusive
+  endif
+  return current_buffer . '/' . total_buffers . label
+endfunction
 
 " Colorize status bar (to distinguish cf3, exercises and regular markdown files)
 " Markdown: black on green
-au BufRead *.md hi StatusLine ctermbg=black ctermfg=green
+"au BufRead *.md hi StatusLine ctermbg=black ctermfg=green
+"au BufRead *.md set laststatus=0
+" display number of current buffer
+" (https://stackoverflow.com/questions/5547943/display-number-of-current-buffer)
+au BufRead *.md set laststatus=2 statusline=%{CustomStatusline()}
+
 " CF3: black on yellow
-au BufRead *.cf hi StatusLine ctermbg=black ctermfg=yellow
+"au BufRead *.cf hi StatusLine ctermbg=black ctermfg=yellow
+"au BufRead *.cf set laststatus=0
+"au BufRead *.cf set laststatus=2 statusline=%!bufnr('#')
 " Exercises: black on red
-au BufRead *.exr.md hi StatusLine ctermbg=black ctermfg=red
+"au BufRead *.exr.md hi StatusLine ctermbg=black ctermfg=red
+au BufRead *.cf     set laststatus=2 statusline=%{CustomStatusline()}
+au BufRead *.exr.md set laststatus=2 statusline=%{CustomStatusline()}
 
 " Hide ~ marks that show past end of text (cosmetic change)
 highlight EndOfBuffer ctermfg=black ctermbg=black
-
 EOF
