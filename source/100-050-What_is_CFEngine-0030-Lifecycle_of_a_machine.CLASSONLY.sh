443ad04d2203131f02c32ea347f4621b483eb0b9
Aleksey Tsalolikhin <aleksey@verticalsysadmin.com>
2021-01-17 22:46:30 +0000

    reorder files

---
+++ source/100-050-What_is_CFEngine-0030-Lifecycle_of_a_machine.CLASSONLY.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo 'Remy Evard, An Analysis of UNIX System Configuration'
+echo 'USENIX Proceedings: Eleventh Systems Administration Conference (LISA 1997), October 26-31, 1997'
+echo
+
+# Display diagram
+/mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe\
+  file://wsl%24/Ubuntu/home/atsaloli/git/cf3-tutorial/images/figures/lifecycle.png
