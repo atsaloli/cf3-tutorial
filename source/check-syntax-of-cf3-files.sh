53d55c294ed35816ddbadac52134c654e3b8e7ca
Aleksey Tsalolikhin <atsalolikhin@spokeo.com>
2021-02-01 01:31:30 +0000

    x

--- source/check-syntax-of-cf3-files.sh
+++ source/check-syntax-of-cf3-files.sh
@@ -2,7 +2,10 @@
 
 # check syntax of all cf3 files
 
-for f in *cf
+
+> cf-syntax-check-failed.txt  # zero out the file
+
+for f in $(ls *cf | grep -v SKIP)
 do
   sudo cf-promises -f ./$f
   if [ $? != 0 ]; then echo $f >> cf-syntax-check-failed.txt; fi
