3f4dd3e801e1e4fae62fc8c102fb96a4425232a9
Aleksey Tsalolikhin <aleksey@verticalsysadmin.com>
2025-12-27 12:00:07 +0000

    add assets for index.html page

---
+++ html/assets/js/custom_domain_js.js
@@ -0,0 +1,34 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+CustomDomain.setup({
+});
+
+}
+/*
+     FILE ARCHIVED ON 10:22:58 Jul 22, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 11:34:11 Dec 27, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.501
+  exclusion.robots: 0.039
+  exclusion.robots.policy: 0.03
+  esindex: 0.008
+  cdx.remote: 11.139
+  LoadShardBlock: 103.114 (3)
+  PetaboxLoader3.datanode: 123.333 (5)
+  PetaboxLoader3.resolve: 77.319 (2)
+  load_resource: 139.337
+  loaddict: 30.117
+*/
